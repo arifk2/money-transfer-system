66e86a708c432832e872cb842f41db5b1df81044
Mohd Arif Khan <moharifk10@gmail.com>
2023-01-17 02:04:56 +0530

    initial commmit money transfer system code

---
+++ src/main/java/com/dxc/mts/api/model/User.java
@@ -0,0 +1,51 @@
+package com.dxc.mts.api.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author mkhan339
+ *
+ */
+@Data
+@NoArgsConstructor
+@Entity(name = "USERS")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "USER_ID")
+	private Long userId;
+
+	@Column(name = "USERNAME")
+	private String userame;
+
+	@Column(name = "PASSWORD")
+	private String password;
+
+	@Column(name = "EMAIL_ADDRESS")
+	private String emailAddress;
+
+	@Column(name = "PHONE_NUMBER")
+	private String phoneNumber;
+
+	@OneToMany(mappedBy = "user")
+	private List<Account> accounts;
+
+	@OneToOne(mappedBy = "user")
+	private Address address;
+
+	@OneToMany(mappedBy = "user")
+	private List<EventLog> eventLog;
+}
