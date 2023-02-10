279379998549e5ff95d04f569410f200c6e025cc
Mohd Arif Khan <moharifk10@gmail.com>
2023-02-10 22:04:45 +0530

    remove account type enums and formatted BaseAppConstants

--- src/main/java/com/dxc/mts/api/enums/BaseAppConstants.java
+++ src/main/java/com/dxc/mts/api/enums/BaseAppConstants.java
@@ -7,11 +7,17 @@ package com.dxc.mts.api.enums;
  */
 public enum BaseAppConstants {
 
-	MM_DD_YYYY_HH_MM_SS_WITHZONE("MM-dd-yyyy'T'HH:mm:ss'Z'"), TIMEZONE_UTC("UTC"), MM_DD_YYYY("MM-dd-yyyy"),
-	INVALID_DATE_FORMAT_MESSAGE(
-			"Requested date format is not valid, and the valid date format is MM-dd-yyyy'T'HH:mm:ss'Z'. If date is not available use 00-00-1971T00:00:00Z."),
-	BACK_SLASH("/"), BLANK(""), NOT_AVAILABLE("N/A"), SAVINGS("SAVINGS"), CURRENT("CURRENT"),
-	AUTHORIZATION("Authorization"), BEARER("Bearer ");
+	MM_DD_YYYY_HH_MM_SS_WITHZONE("MM-dd-yyyy'T'HH:mm:ss'Z'"), 
+	TIMEZONE_UTC("UTC"), 
+	MM_DD_YYYY("MM-dd-yyyy"),
+	INVALID_DATE_FORMAT_MESSAGE("Requested date format is not valid, and the valid date format is MM-dd-yyyy'T'HH:mm:ss'Z'. If date is not available use 00-00-1971T00:00:00Z."),
+	BACK_SLASH("/"), 
+	BLANK(""),
+	NOT_AVAILABLE("N/A"), 
+	SAVINGS("SAVINGS"), 
+	CURRENT("CURRENT"),
+	AUTHORIZATION("Authorization"), 
+	BEARER("Bearer ");
 
 	private String value;
 
